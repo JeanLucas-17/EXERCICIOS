2a87e9136d542b64285da36f942e7322cdd2e012
JeanLucas-17 <jeanlucasts131@gmail.com>
2026-05-18 09:44:26 -0300

    Delete exercicios.py directory

--- exercicios.py/MUNDO 2/ex053.py
+++
@@ -1,7 +0,0 @@
-frase = str(input("Digite uma frase: ")).lower().replace(" ", "")
-inverso = frase[::-1]
-
-if frase == inverso:
-    print(f"A frase '{frase}' é palindromo!")
-else:
-    print(f"A farse '{frase}' não é palindromo!")
